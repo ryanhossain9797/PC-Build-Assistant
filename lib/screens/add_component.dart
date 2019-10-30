cf9154f21a18312bf414a1192274b10087134e77
ryanhossain9797 <ryanhossain9797@gmail.com>
2019-10-31 00:35:34 +0600

    Secret Stuff SHHHHH

--- lib/screens/add_component.dart
+++ lib/screens/add_component.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:pc_build_assistant/components/rounded_button_widget.dart';
 import 'package:pc_build_assistant/constants.dart';
-import 'package:pc_build_assistant/models/pc_component.dart';
 
 class AddScreen extends StatefulWidget {
   static String id = "/addScreenId";
@@ -15,6 +14,7 @@ class _AddScreenState extends State<AddScreen> {
   String _manufacturer = "";
   String _name = "";
   String _description = "";
+  String _imgurl;
   Firestore _storage = Firestore.instance;
 
   @override
@@ -44,27 +44,24 @@ class _AddScreenState extends State<AddScreen> {
                 _description = description;
               },
             ),
+            TextField(
+              decoration: InputDecoration(hintText: "Image Url"),
+              onChanged: (imgurl) {
+                _imgurl = imgurl;
+              },
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: RoundedButton(
                 title: "Add",
                 color: kRegisterButtonColor,
                 onPressed: () {
-                  PCComponentModel model = PCComponentModel(
-                    name: _name,
-                    manufacturer: _manufacturer,
-                    description: _description,
-                  );
-                  print(model.manufacturer +
-                      " " +
-                      model.name +
-                      " " +
-                      model.description);
                   _storage.collection('pc-components').add(
                     {
                       "manufacturer": _manufacturer,
                       "name": _name,
-                      "description": _description
+                      "description": _description,
+                      "imgurl": _imgurl,
                     },
                   );
                 },
